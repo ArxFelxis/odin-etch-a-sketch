39e9c06dcad0d413e1b34e9b72f5b58670860ef4
ArxFelxis <176923291+ArxFelxis@users.noreply.github.com>
2025-11-05 12:32:15 +0800

    Allow custom grids

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const grid = document.querySelector(".grid")
-const btn = document.querySelector(".num-grid")
+const btn = document.querySelector(".num-grid")  
 
-for (let i = 0; i < 256; i++) {
+function getGridSize(n) {
+    // Get number of tiles to loop
+    for (let i = 0; i < n*(n+1); i++) {
     const div = document.createElement("div")
     grid.appendChild(div)
-}   
+
+    // Indicate number of tiles per row & number of rows
+    document.querySelectorAll('.grid > div')
+    .forEach((el, index) => {
+    if (index % (n+1) === 0) {
+    el.style.setProperty('width', '100%', 'important')
+    el.style.border = "0"
+    el.style.height = "0"
+    } else {
+        el.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "grey"
+    })
+    //     el.addEventListener("mouseout", (event) => {
+    //         event.target.style.backgroundColor = "white"
+    // })
+}})
+}
+}
+
+btn.addEventListener("click", () => {
+    if (document.querySelectorAll('.grid > div')) {
+        (document.querySelectorAll('.grid > div')).forEach((el) => {
+            el.remove()
+        })
+    }
+    let num = Number(prompt("Please enter your grid size."))
+    if (num >= 100) {
+        return alert("Please choose a smaller grid.")
+    } else {
+        return getGridSize(num)
+    }
+})
